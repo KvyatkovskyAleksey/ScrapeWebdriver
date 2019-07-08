2734f61bfd173784c21c0716587a8611d847a34f
KvyatkovskyAleksey <kvyatkovsky@mail.ru>
2019-07-08 19:12:30 +0300

    0.3 change_proxy bug

--- scrape_webdriver.py
+++ scrape_webdriver.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 
 class ScrapyWebdriver(webdriver.Firefox):
-	'''Class with methods for scraping on selenium(Firefox) base'''
+	'''Class with methods for scraping on selenium.webdriver(Firefox) base'''
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
 		    
@@ -15,27 +15,28 @@ class ScrapyWebdriver(webdriver.Firefox):
 		proxy = proxy.split(':')
 		self.get('about:config')
 		if 'socks' in proxy[0].lower():
-			set_preference(self, 'network.proxy.socks_version', int(proxy[0][-1]))
-			set_preference(self, 'network.proxy.socks', proxy[1])
-			set_preference(self, 'network.proxy.socks_port', int(proxy[2]))
-			set_preference(self, 'network.proxy.type', 1)
+			self.set_preference('network.proxy.socks_version', int(proxy[0][-1]))
+			self.set_preference('network.proxy.socks', proxy[1])
+			self.set_preference('network.proxy.socks_port', int(proxy[2]))
+			self.set_preference('network.proxy.type', 1)
 		elif 'https' in proxy[0].lower():
-			set_preference(self, 'network.proxy.ssl', proxy[1])
-			set_preference(self, 'network.proxy.ssl_port', int(proxy[2]))
-			set_preference(self, 'network.proxy.type', 1)
+			self.set_preference('network.proxy.ssl', proxy[1])
+			self.set_preference('network.proxy.ssl_port', int(proxy[2]))
+			self.set_preference('network.proxy.type', 1)
 		elif 'http' in proxy[0].lower():
-			set_preference(self, 'network.proxy.http', proxy[1])
-			set_preference(self, 'network.proxy.ftp', proxy[1])
-			set_preference(self, 'network.proxy.socks', proxy[1])
-			set_preference(self, 'network.proxy.ssl', proxy[1])
-			set_preference(self, 'network.proxy.http_port', int(proxy[2]))
-			set_preference(self, 'network.proxy.ftp_port', int(proxy[2]))
-			set_preference(self, 'network.proxy.socks_port', int(proxy[2]))
-			set_preference(self, 'network.proxy.ssl_port', int(proxy[2]))
-			set_preference(self, 'network.proxy.type', 1)
-			set_preference(self, 'network.proxy.share_proxy_settings', 'true')
+			self.set_preference('network.proxy.http', proxy[1])
+			self.set_preference('network.proxy.ftp', proxy[1])
+			self.set_preference('network.proxy.socks', proxy[1])
+			self.set_preference('network.proxy.ssl', proxy[1])
+			self.set_preference('network.proxy.http_port', int(proxy[2]))
+			self.set_preference('network.proxy.ftp_port', int(proxy[2]))
+			self.set_preference('network.proxy.socks_port', int(proxy[2]))
+			self.set_preference('network.proxy.ssl_port', int(proxy[2]))
+			self.set_preference('network.proxy.type', 1)
+			self.set_preference('network.proxy.share_proxy_settings', 'true')
 
 	def set_preference(self, pref, params):
+		'''Set preference in 'about:config' '''
 		if params in ['false', 'true']:
 			print('Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch).setBoolPref("'+pref+'", '+str(params)+');')
 			self.execute_script('Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch).setBoolPref("'+pref+'", '+str(params)+');')
