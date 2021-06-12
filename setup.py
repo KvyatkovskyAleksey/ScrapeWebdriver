868eda18bef3393c820807451955cd4dbbd9361d
aleksey <kvyatkovsky89@gmail.com>
2021-06-12 17:10:15 +0300

    Udpated setup.py

--- setup.py
+++ setup.py
@@ -8,19 +8,15 @@ setuptools.setup(
 	version="0.34",
 	author="Aleksey Kvyatkovsky",
 	author_email="kvyatkovsky@mail.ru",
-	description="Class based on selenium webdriver.Firefox with methods\
-	 for scraping.",
+	description="Class based on selenium webdriver.Firefox with methods  for scraping.",
 	long_description=long_description,
 	long_description_content_type="text/markdown",
 	url="https://github.com/KvyatkovskyAleksey/ScrapeWebdriver",
 	packages=setuptools.find_packages(),
 	install_requires=[
-        'beautifulsoup4==4.7.1',
-		'bs4==0.0.1',
-		'pkg-resources==0.0.0',
+		'beautifulsoup4==4.9.3',
 		'selenium==3.141.0',
-		'soupsieve==1.9.2',
-		'urllib3==1.25.3'
+		'webdriver-manager==3.4.2'
     ],
 	classifiers=[
         "Programming Language :: Python :: 3",
