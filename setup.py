9c1e471c6d8a21cf70d752580b33a3b7b5adc998
aleksey <kvyatkovsky89@gmail.com>
2021-06-13 23:42:03 +0300

    Added MANIFEST.in

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name="scrapy_webdriver",
-	version="0.41",
+	version="0.42",
 	author="Aleksey Kvyatkovsky",
 	author_email="kvyatkovsky@mail.ru",
 	description="Class based on selenium webdriver.Firefox with methods  for scraping.",
@@ -19,6 +19,9 @@ setuptools.setup(
 		'webdriver-manager==3.4.2',
 		'Scrapy==2.5.0'
     ],
+	package_data={'': ['license.txt']},
+	include_package_data=True,
+
 	classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
